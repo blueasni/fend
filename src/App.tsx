6e6572d985205db9e9bd295678983b1767b98736
Asnake Ayele <blue.asni@gmail.com>
2023-06-09 15:31:05 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { FormControl, FormHelperText, Input, InputLabel, TextField, ThemeProvider } from '@mui/material';
 
-function App() {
+
+const App = (): JSX.Element => {
+  const [clickedButton, setClickedButton] = useState('');
+
+  const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    const button: HTMLButtonElement = event.currentTarget;
+    setClickedButton(button.name);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3>Kindacode.com</h3>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Email address</InputLabel>
+        <Input id="my-input" aria-describedby="my-helper-text" />
+        <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+      </FormControl>
+
+      <form>
+
+        <button onClick={buttonHandler} className="button" name="button 1">
+          Button 1
+        </button>
+
+        <button onClick={buttonHandler} className="button" name="button 2">
+          Button 2
+        </button>
+
+        <button onClick={buttonHandler} className="button" name="button 3">
+          Button 3
+        </button>
+
+        <button onClick={buttonHandler} className="button" name="button 4">
+          Button 4
+        </button>
+
+      </form>
+      <h1>
+        {clickedButton !== ""
+          ? `You have clicked "${clickedButton}"`
+          : "No button clicked yet"}
+      </h1>
     </div>
+
   );
-}
+
+};
 
 export default App;
---
+++ src/components/List.tsx
@@ -0,0 +1,10 @@
+
+import { FC } from 'react'
+
+const List: FC<{ data: { name: string }[] }> = ({ data }) => (
+    <ul>
+      {data.map(({ name }) => <li>{name}</li>)}
+    </ul>
+  )
+
+export default List
